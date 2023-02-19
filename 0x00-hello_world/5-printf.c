4715524098c00d942d708a7bbb387505a114b6fa
Lesedi <lesedimokobane@outlook.com>
2023-02-19 09:49:22 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc printf (with proper grammar, but the outcome is a piece of art, \n)
+#include<stdio.h>
+/*Main-entry point
+ *
+ * Description: Simple print with printf function
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
