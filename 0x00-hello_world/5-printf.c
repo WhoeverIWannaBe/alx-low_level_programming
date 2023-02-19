4182c7de014c78b768cef31fa12bff7767e09488
Lesedi <lesedimokobane@outlook.com>
2023-02-19 09:51:54 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-/*Main-entry point
+/*
+ * Main-entry point
  *
  * Description: Simple print with printf function
  *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
 	return (0);
 }
