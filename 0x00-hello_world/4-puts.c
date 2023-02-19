d19f97ab30042f59bc1024a94033c52407e37eeb
Lesedi <lesedimokobane@outlook.com>
2023-02-19 09:44:36 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main(void)/*This servces to compile our program*/
+/*
+ *main - entry point
+ *
+ * Description : Simple print using puts function
+ *
+ * return: always 0 (Success)
+ *
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
