6747ad416d2ef5549f99db60f3928967c59adea0
Lesedi <lesedimokobane@outlook.com>
2023-02-20 13:58:09 -0800

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,16 @@
 int main(void)
 {
 	float f;
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long: %zu byte(s)\n", sizeof(l));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+
 	return (0);
 }
