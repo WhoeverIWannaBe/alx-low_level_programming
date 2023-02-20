344804f2ded56991bac1e139814a21f70366ec90
Lesedi <lesedimokobane@outlook.com>
2023-02-20 14:26:15 -0800

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 int main(void)
 {
-	fprintf(stderr, "and that piece of art in=s useful\" - Dora Korpar, 2015-10-19\n");
-	
-	return (0);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 }	
